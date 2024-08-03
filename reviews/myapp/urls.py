741630b721cf65842da2de4a12cc573a9d1d3c83
cristovdm <civandemeer@miuandes.cl>
2024-08-02 20:18:21 -0400

    first committ

--- reviews/myapp/urls.py
+++ reviews/myapp/urls.py
@@ -10,20 +10,28 @@ urlpatterns = [
     
 
     path('books/', views.BookListView.as_view(), name='book_list'),
-    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
-    path('books/create/', views.BookCreateView.as_view(), name='book_create'),
-    path('books/<int:pk>/update/', views.BookUpdateView.as_view(), name='book_update'),
-    path('books/<int:pk>/delete/', views.BookDeleteView.as_view(), name='book_delete'),
 
     path('reviews/', views.ReviewListView.as_view(), name='review_list'),
-    path('reviews/<int:pk>/', views.ReviewDetailView.as_view(), name='review_detail'),
-    path('reviews/create/', views.ReviewCreateView.as_view(), name='review_create'),
-    path('reviews/<int:pk>/update/', views.ReviewUpdateView.as_view(), name='review_update'),
-    path('reviews/<int:pk>/delete/', views.ReviewDeleteView.as_view(), name='review_delete'),
 
     path('sales/', views.SaleListView.as_view(), name='sale_list'),
-    path('sales/<int:pk>/', views.SaleDetailView.as_view(), name='sale_detail'),
-    path('sales/create/', views.SaleCreateView.as_view(), name='sale_create'),
-    path('sales/<int:pk>/update/', views.SaleUpdateView.as_view(), name='sale_update'),
-    path('sales/<int:pk>/delete/', views.SaleDeleteView.as_view(), name='sale_delete'),
+
+
+    # Las de abajo todavía no son implementadas.
+
+    path('books/<int:pk>/', views.BookListView.as_view(), name='book_detail'),
+    path('books/create/', views.BookListView.as_view(), name='book_create'),
+    path('books/<int:pk>/update/', views.BookListView.as_view(), name='book_update'),
+    path('books/<int:pk>/delete/', views.BookListView.as_view(), name='book_delete'),
+
+    
+    path('reviews/<int:pk>/', views.BookListView.as_view(), name='review_detail'),
+    path('reviews/create/', views.BookListView.as_view(), name='review_create'),
+    path('reviews/<int:pk>/update/', views.BookListView.as_view(), name='review_update'),
+    path('reviews/<int:pk>/delete/', views.BookListView.as_view(), name='review_delete'),
+
+    path('sales/', views.BookListView.as_view(), name='sale_list'),
+    path('sales/<int:pk>/', views.BookListView.as_view(), name='sale_detail'),
+    path('sales/create/', views.BookListView.as_view(), name='sale_create'),
+    path('sales/<int:pk>/update/', views.BookListView.as_view(), name='sale_update'),
+    path('sales/<int:pk>/delete/', views.BookListView.as_view(), name='sale_delete'),
 ]
--- reviews/myapp/views.py
+++ reviews/myapp/views.py
@@ -35,100 +35,19 @@ class AuthorDeleteView(DeleteView):
     template_name = 'author_confirm_delete.html'
     success_url = reverse_lazy('author_list')
 
-
-
-
-"""
-class AuthorListView(ListView):
-    model = Author
-    template_name = 'author_list.html'
-
-class AuthorDetailView(DetailView):
-    model = Author
-    template_name = 'myapp/author_detail.html'
-
-class AuthorCreateView(CreateView):
-    model = Author
-    template_name = 'myapp/author_form.html'
-    fields = ['name', 'date_of_birth', 'country_of_origin', 'short_description']
-
-class AuthorUpdateView(UpdateView):
-    model = Author
-    template_name = 'myapp/author_form.html'
-    fields = ['name', 'date_of_birth', 'country_of_origin', 'short_description']
-
-class AuthorDeleteView(DeleteView):
-    model = Author
-    template_name = 'myapp/author_confirm_delete.html'
-    success_url = reverse_lazy('author_list')
-"""
-
-# Repite para Book, Review y Sale
+# Books CRUD (Por el momento solo tiene la Lista)
 class BookListView(ListView):
     model = Book
-    template_name = 'myapp/book_list.html'
+    template_name = 'book_list.html'
 
-class BookDetailView(DetailView):
-    model = Book
-    template_name = 'myapp/book_detail.html'
-
-class BookCreateView(CreateView):
-    model = Book
-    template_name = 'myapp/book_form.html'
-    fields = ['name', 'summary', 'date_of_publication', 'number_of_sales', 'author']
-
-class BookUpdateView(UpdateView):
-    model = Book
-    template_name = 'myapp/book_form.html'
-    fields = ['name', 'summary', 'date_of_publication', 'number_of_sales', 'author']
-
-class BookDeleteView(DeleteView):
-    model = Book
-    template_name = 'myapp/book_confirm_delete.html'
-    success_url = reverse_lazy('book_list')
-
+# Reviews CRUD (Por el momento solo tiene la Lista)
 class ReviewListView(ListView):
     model = Review
-    template_name = 'myapp/review_list.html'
+    template_name = 'review_list.html'
 
-class ReviewDetailView(DetailView):
-    model = Review
-    template_name = 'myapp/review_detail.html'
 
-class ReviewCreateView(CreateView):
-    model = Review
-    template_name = 'myapp/review_form.html'
-    fields = ['book', 'review', 'score', 'number_of_upvotes']
-
-class ReviewUpdateView(UpdateView):
-    model = Review
-    template_name = 'myapp/review_form.html'
-    fields = ['book', 'review', 'score', 'number_of_upvotes']
-
-class ReviewDeleteView(DeleteView):
-    model = Review
-    template_name = 'myapp/review_confirm_delete.html'
-    success_url = reverse_lazy('review_list')
-
+# Sales CRUD (Por el momento solo tiene la Lista)
 class SaleListView(ListView):
     model = Sale
-    template_name = 'myapp/sale_list.html'
+    template_name = 'sale_list.html'
 
-class SaleDetailView(DetailView):
-    model = Sale
-    template_name = 'myapp/sale_detail.html'
-
-class SaleCreateView(CreateView):
-    model = Sale
-    template_name = 'myapp/sale_form.html'
-    fields = ['book', 'year', 'sales']
-
-class SaleUpdateView(UpdateView):
-    model = Sale
-    template_name = 'myapp/sale_form.html'
-    fields = ['book', 'year', 'sales']
-
-class SaleDeleteView(DeleteView):
-    model = Sale
-    template_name = 'myapp/sale_confirm_delete.html'
-    success_url = reverse_lazy('sale_list')
