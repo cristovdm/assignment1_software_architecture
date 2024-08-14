4bc57407f5da9789038839ba7264e076c4488ac7
amparoq <aquiroz@miuandes.cl>
2024-08-14 11:51:17 -0400

    docker listo

--- reviews/myproject/settings.py
+++ reviews/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vsbo%nn-rq496-hn+#b1_d8g2k1sc%3i-hx*ag(r%xop1+#j0p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
